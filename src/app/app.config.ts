7312b9a869b4d1401aba5f75489199ac9d457125
sayeed <md.sayeed2@det.nsw.edu.au>
2026-05-20 16:00:37 +1000

    chore: add ESLint and Prettier

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,5 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
-import {
-  provideRouter,
-  withComponentInputBinding,
-  withViewTransitions,
-} from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
