3a4714b723638c1d2c24e86bcdb7e8d3b3ce378c
sayeed <md.sayeed2@det.nsw.edu.au>
2026-05-21 14:09:09 +1000

    feat: add home page

--- src/app/features/home/pages/home.page.ts
+++ src/app/features/home/pages/home.page.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  template: `<p>Home</p>`,
+  imports: [RouterLink],
+  templateUrl: './home.page.html',
+  styleUrl: './home.page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage {}
